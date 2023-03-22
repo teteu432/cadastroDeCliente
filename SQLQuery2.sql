de3b58aa399360cd85aa897f3a4d08cbda0bd032
Mateus da Silva <84821352+teteu432@users.noreply.github.com>
2023-03-21 21:15:30 -0300

    Sistema de cadastro funcionando

---
+++ SQLQuery2.sql
@@ -0,0 +1,20 @@
+SELECT *
+FROM person.Person;
+
+SELECT Title
+from person.person;
+
+SELECT * 
+FROM person.EmailAddress;
+
+SELECT FirstName
+FROM person.person;
+	
+SELECT firstName, LastName
+FROM person.person;
+	
+SELECT DISTINCT FirstName
+FROM person.person
+
+SELECT DISTINCT LastName
+FROM person.person
